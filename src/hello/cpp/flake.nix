8fd5f64ad0964ad61da61aed0da8715622abcb3a
Tobias Ritzau <tobias@ritzau.se>
2025-08-20 15:58:59 +0200

    Rename solutions -> src

---
+++ src/hello/cpp/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Hello World C++";
+
+  inputs = {
+    aoc-langs.url = "github:ritzau/aoc-polyglot-languages";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      aoc-langs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      aoc-langs.lib.${system}.c.mkStandardOutputs {
+        src = ./.;
+        pname = "hello-cpp";
+      }
+    );
+}
