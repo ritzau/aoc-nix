c9c245cf3f56ffbee6f3b33c62fe23f6fbe9d710
Tobias Ritzau <tobias@ritzau.se>
2025-08-21 08:53:45 +0200

    Fix Java flake to use correct description syntax

--- src/hello/java/flake.nix
+++ src/hello/java/flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Hello World Java";
-
   inputs = {
     aoc-langs.url = "github:ritzau/aoc-polyglot-languages";
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,7 +7,7 @@
   outputs =
     { aoc-langs, ... }:
     aoc-langs.lib.mkSimpleFlake ./. {
-      inherit description;
+      description = "Hello World Java";
       language = "java";
     };
 }
