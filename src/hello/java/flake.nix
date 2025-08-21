7f466fe4156dae70078c212cbedf27cf5f9f54c0
Tobias Ritzau <tobias@ritzau.se>
2025-08-21 09:06:59 +0200

    Convert Java flake to secure language-specific API with auto-extraction

--- src/hello/java/flake.nix
+++ src/hello/java/flake.nix
@@ -1,13 +1,16 @@
 {
+  description = "Hello World Java";
+
   inputs = {
     aoc-langs.url = "github:ritzau/aoc-polyglot-languages";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { aoc-langs, ... }:
-    aoc-langs.lib.mkSimpleFlake ./. {
-      description = "Hello World Java";
-      language = "java";
+    { self, aoc-langs, ... }:
+    aoc-langs.lib.java.mkSimpleFlake self {
+      # description auto-extracted from self.description
+      # src auto-extracted from self.outPath
+      # jdk defaults to "jdk21"
     };
 }
