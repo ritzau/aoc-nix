a73f2a626d4308b402e1b45f1815e6d514ad9a8c
Tobias Ritzau <tobias@ritzau.se>
2025-08-21 22:28:00 +0200

    Enable ObjC on macOS

--- src/hello/objc/flake.nix
+++ src/hello/objc/flake.nix
@@ -1,20 +1,15 @@
 {
   description = "Hello World Objective-C";
+
   inputs = {
-    aoc-langs.url = "github:ritzau/aoc-polyglot-languages";
-    flake-utils.url = "github:numtide/flake-utils";
+    polyglot.url = "github:ritzau/aoc-polyglot-languages";
+    # flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs =
-    {
-      self,
-      aoc-langs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      aoc-langs.lib.${system}.objc.mkStandardOutputs {
-        src = ./.;
-        pname = "hello-objc";
-      }
-    );
+    { self, polyglot, ... }:
+    polyglot.lib.objc.mkDefaultOutputs {
+      inherit (self) description;
+      src = ./.;
+    };
 }
